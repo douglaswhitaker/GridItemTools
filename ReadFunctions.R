7430a0ace4caff6483f6b625a8f06f08c4107450
Bailey Drew <bailey-drew@hotmail.com>
2020-05-24 13:49:32 -0300

    Added "read.lime.grid" function to read grid data. Also re-coded "read.lime.nongrid" function to be more compact.

--- ReadFunctions.R
+++ ReadFunctions.R
@@ -1,50 +1,43 @@
+###Read the csv file
 dat.raw <-  read.csv("data/real/results-survey945827-202004302207-codes.csv",
                      stringsAsFactors = FALSE)
 
-FixNonGridCode <- function(dat, cols, rows, ...){
-  for (i in cols){
-    for (j in rows){
-      if (dat[j,i]=='D4'){
-        dat[j,i] <- 1
-      }
-      if (dat[j,i]=='D3'){
-        dat[j,i] <- 2
-      }
-      if (dat[j,i]=='D2'){
-        dat[j,i] <- 3
-      }
-      if (dat[j,i]=='D1'){
-        dat[j,i] <- 4
-      }
-      if (dat[j,i]=='A1'){
-        dat[j,i] <- 6
-      }
-      if (dat[j,i]=='A2'){
-        dat[j,i] <- 7
-      }
-      if (dat[j,i]=='A3'){
-        dat[j,i] <- 8
-      }
-      if (dat[j,i]=='A4'){
-        dat[j,i] <- 9
-      }
-      if (dat[j,i]=='N0'){
-        dat[j,i] <- 5
+###Function to read data frome -codes.csv file for likert items
+read.lime.nongrid <- function (dat){
+  code <- c('D4','D3','D2','D1','N0','A1','A2','A3','A4')
+  numcode <- c(1,2,3,4,5,6,7,8,9)
+
+  #Removes grid columns
+  dat <- dat[,which(!sapply(names(dat),
+                              grepl,pattern="_",
+                              simplify=TRUE))]
+
+  #Choose likert columns
+  dat <- dat[,c(13:21,25:34)]
+
+  #Loop to assign numbers to codes
+  for (i in 1:ncol(dat)){
+    for (j in 1:nrow(dat)){
+     for (k in 1:length(code)){
+        if(dat[j,i]==code[k]){
+          dat[j,i] <- numcode[k]
+        }
       }
     }
   }
   return(dat)
 }
 
-read.lime.nongrid <- function(dat1){
-  dat1 <- dat1[,which(!sapply(names(dat1),
+#Test out read.lime.nongrid function
+nongriddata <- read.lime.nongrid(dat.raw)
+
+###Function to read data frome -codes.csv file for grid items
+read.lime.grid <- function(dat){
+  #Removes non-grid columns
+  dat <- dat[,which(sapply(names(dat),
                               grepl,pattern="_",
                               simplify=TRUE))]
-  dat1 <- dat1[,c(13:21,25:34)]
-  dat1 <- FixNonGridCode(dat1,
-                         cols=1:ncol(dat1),
-                         rows=1:nrow(dat1))
-  return (dat1)
 }
 
-dat.raw <- read.lime.nongrid(dat.raw)
+#Test out read.lime.grid function
+griddata <- read.lime.grid(dat.raw)
