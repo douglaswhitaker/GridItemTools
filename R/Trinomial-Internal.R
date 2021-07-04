f37c36a8a6e17c3fb390391714a32652f7426a1b
Douglas Whitaker <douglas.whitaker@gmail.com>
2021-07-04 15:07:39 -0300

    Bug fix for old code (prob.nd). Need to bring it up to date now.

--- R/Trinomial-Internal.R
+++ R/Trinomial-Internal.R
@@ -2,15 +2,55 @@
 # Nd is the difference between N+ and N-
 # The formula is given by (Bian et al., 2011, p. 1156)
 prob.nd <- function(n,nd,k,p_tie){
-  factorial(n)/(factorial(nd+k)*factorial(k)*factorial(n-nd-2*k))*
-    ((1-p_tie)/2)^(nd+2*k)*
-    p_tie^(n-nd-2*k)
+  ########################################################################
+  # Old code that matches the described probability distribution in Bian #
+  ########################################################################
+  factorial(n) /
+    (factorial(nd + k) * factorial(k) * factorial(n - nd - 2*k)) *
+    ((1 - p_tie) / 2)^(nd + 2*k) *
+    p_tie^(n - nd - 2*k)
+  
+  #############################################################
+  # New code that supports larger values of n by using choose #
+  #############################################################
+  # choose(n, nd + 2*k) * prod((nd + k + 1):(nd + 2*k)) / factorial(k) *
+  #   ((1 - p_tie) / 2)^(nd + 2*k) *
+  #   p_tie^(n - nd - 2*k)
 }
 
-prob.nd.cumsum <- function(n,nd,p_tie){
+prob.nd2 <- function(n,nd,k,p_tie){
+  ########################################################################
+  # Old code that matches the described probability distribution in Bian #
+  ########################################################################
+  # factorial(n) /
+  #   (factorial(nd + k) * factorial(k) * factorial(n - nd - 2*k)) *
+  #   ((1 - p_tie) / 2)^(nd + 2*k) *
+  #   p_tie^(n - nd - 2*k)
+  
+  #############################################################
+  # New code that supports larger values of n by using choose #
+  #############################################################
+  choose(n, nd + 2*k) * 
+    ifelse(k == 0, 
+           yes = 1, 
+           no = prod((nd + k + 1):(nd + 2*k))) / 
+    factorial(k) *
+    ((1 - p_tie) / 2)^(nd + 2*k) *
+    p_tie^(n - nd - 2*k)
+}
+
+prob.nd.cumsum <- function(n,nd,p_tie, version2 = TRUE, verbose = FALSE){
   tmp.prob <- 0
   for (k in 0:((n-nd)/2)){
-    tmp.prob <- tmp.prob + prob.nd(n=n,nd=nd,k=k,p_tie=p_tie)
+    if (version2){
+      tmp.prob <- tmp.prob + prob.nd2(n=n,nd=nd,k=k,p_tie=p_tie)
+    }
+    else {
+      tmp.prob <- tmp.prob + prob.nd(n=n,nd=nd,k=k,p_tie=p_tie)
+    }
+  }
+  if (verbose){
+    print(paste("tmp.prob=",tmp.prob," n=",n," nd=",nd," p_tie=",p_tie),sep="")
   }
   return(tmp.prob)
 }
